dee51c11bd80df15cffcf191bace83f52c610cdd
Trini-d <trinidad.dominguez66@gmail.com>
2019-07-02 09:27:28 -0300

    Primera confuguración pokedesk y filtros

--- src/data.js
+++ src/data.js
@@ -3,8 +3,53 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
+/*
 const example = () => {
   return 'example';
 };
 
 window.example = example;
+*/
+/* Manejo del DOM */
+//llamando toda la data de pokemon
+
+const datos = POKEMON.pokemon;
+//creando una variable vacia donde se guardaran solo los nombres desde la data pokemon/
+
+function createTable(datos){
+  let result = "<table>";
+  let cantPokemons = 0;
+
+  for (let i = 0; i< datos.length; i++) {
+      if(cantPokemons == 0){
+        result += "<tr>"
+      }
+      result += "<td>"
+      result += '<img id ="imagenes" src="'+datos[i].img+'" alt="'+datos[i].name+'" width="110px" onclick="showInfo('+i+')"></img>';
+      result += "<br>"
+      result += "#"+datos[i].id;
+      result += "<div id='poke-"+i+"'></div>"
+      result += "</td>"  
+      cantPokemons++;
+      if(cantPokemons == 9){
+        result += "</tr>"
+        cantPokemons = 0;
+      }
+    }
+  result +="</table>";
+  return result;
+}
+
+function showInfo(i){
+  if(document.getElementById("poke-"+i).innerHTML==""){
+    let result= datos[i].name;
+    result += "<br>"
+    result += "Debilidad: "+datos[i].weaknesses;
+    document.getElementById("poke-"+i).innerHTML = result;
+  }else{
+    document.getElementById("poke-"+i).innerHTML = "";
+  }  
+  return showInfo();
+}
+
+//hacer que el boton show ejecute la accion de mostrar solo los nombres/
--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 /* Manejo del DOM */
+//hacer que el boton show ejecute la accion de mostrar solo los nombres/
+
+
+document.getElementById("test-button").addEventListener("click", ()=>{
+    document.getElementById("empty-container").innerHTML = createTable(datos);
+  })
+  document.getElementById("primera-evolución").addEventListener("click", ()=>{
+    const result = datos.filter(pokemon => pokemon.next_evolution && !pokemon.prev_evolution);
+    document.getElementById("empty-container").innerHTML = createTable(result);
+  });
+  document.getElementById("segunda-evolución").addEventListener("click", ()=>{
+    const result = datos.filter(pokemon => pokemon.prev_evolution && pokemon.prev_evolution.length == 1);
+    document.getElementById("empty-container").innerHTML = createTable(result);
+  });
+  document.getElementById("tercera-evolución").addEventListener("click", ()=>{
+    const result = datos.filter(pokemon => pokemon.prev_evolution && pokemon.prev_evolution.length == 2);
+    document.getElementById("empty-container").innerHTML = createTable(result);
+  });
+  
+  document.getElementById("candies").addEventListener("change", ()=>{
+    const value = document.getElementById("candies").value
+    let result = datos.filter(pokemon => pokemon.egg == value);
+    document.getElementById("empty-container").innerHTML = createTable(result);
+  
+  })
+  //crea la tabla con todos los pokemons cuando se inicia la pagina
+  document.getElementById("empty-container").innerHTML = createTable(datos);
