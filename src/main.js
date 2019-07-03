75bc5c36e08e1d17c7c52cd52ba8d22fc8100d87
Trini-d <trinidad.dominguez66@gmail.com>
2019-07-03 09:56:25 -0300

    Configuración de PokeDex

--- src/main.js
+++ src/main.js
@@ -2,20 +2,89 @@
 //hacer que el boton show ejecute la accion de mostrar solo los nombres/
 
 
+const datos = POKEMON.pokemon;
+
+
+for (let i = 0; i< datos.length; i++){
+
+  let pokemonInfo = document.createElement("div");
+  pokemonInfo.id = datos[i].name;
+  pokemonInfo.className = "pokeInfo";
+
+  let pokemonName = document.createElement("p");
+  pokemonName.textContent = datos[i].name;
+
+  let pokemonImagen = document.createElement("img");
+  pokemonImagen.src = datos[i].img;
+  pokemonImagen.className = "pokeImagen";
+
+  let pokemonId = document.createElement("p");
+  pokemonId.textContent = datos[i].id;
+
+  pokemonInfo.appendChild(pokemonName);
+  pokemonInfo.appendChild(pokemonImagen);
+  pokemonInfo.appendChild(pokemonId);
+
+  document.getElementById("pokemon-container").appendChild(pokemonInfo).innerHTML;
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//creando una variable vacia donde se guardaran solo los nombres desde la data pokemon/
+/*
+function createTable(datos){
+  let result = "<table>";
+  let cantPokemons = 0;
+
+  for (let i = 0; i< datos.length; i++) {
+      if(cantPokemons == 0){
+        result += "<tr>"
+      }
+      result += "<td>"
+      result += '<img id ="imagenes" src="'+datos[i].img+'"  width="110px" onclick="showInfo('+i+')"></img>';
+      result += "<br>"
+      result += "#"+datos[i].id;
+      result += "<div id='poke-"+i+"'></div>"
+      result += "</td>"  
+      cantPokemons++;
+      if(cantPokemons == 9){
+        result += "</tr>"
+        cantPokemons = 0;
+      }
+    }
+  result +="</table>";
+  return result;
+};
+
 document.getElementById("test-button").addEventListener("click", ()=>{
     document.getElementById("empty-container").innerHTML = createTable(datos);
   })
   document.getElementById("primera-evolución").addEventListener("click", ()=>{
-    const result = datos.filter(pokemon => pokemon.next_evolution && !pokemon.prev_evolution);
-    document.getElementById("empty-container").innerHTML = createTable(result);
+    document.getElementById("empty-container").innerHTML = createTable(resultFirstEvolution);
   });
   document.getElementById("segunda-evolución").addEventListener("click", ()=>{
-    const result = datos.filter(pokemon => pokemon.prev_evolution && pokemon.prev_evolution.length == 1);
-    document.getElementById("empty-container").innerHTML = createTable(result);
+  document.getElementById("empty-container").innerHTML = createTable(resultSecondEvolution);
   });
   document.getElementById("tercera-evolución").addEventListener("click", ()=>{
-    const result = datos.filter(pokemon => pokemon.prev_evolution && pokemon.prev_evolution.length == 2);
-    document.getElementById("empty-container").innerHTML = createTable(result);
+  document.getElementById("empty-container").innerHTML = createTable(resultThirdEvolution);
   });
   
   document.getElementById("candies").addEventListener("change", ()=>{
@@ -25,4 +94,9 @@ document.getElementById("test-button").addEventListener("click", ()=>{
   
   })
   //crea la tabla con todos los pokemons cuando se inicia la pagina
-  document.getElementById("empty-container").innerHTML = createTable(datos);
+  
+
+  document.getElementById("llamandoImagenes").addEventListener("click", ()=>{
+    document.getElementById("poke-").innerHTML =showInfo(i);
+  })
+*/
